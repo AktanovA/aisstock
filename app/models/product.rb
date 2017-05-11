bac38c0a34dfc9bfef26f0ff04b59b96de45d694
AktanovA <amir.aktanov@gmail.com>
2017-05-12 02:52:35 +0300

    Add total price

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
 	validates :title, presence: true,
                     length: { minimum: 4, maximum: 25}
     validates :quantity, :price, :supplier, presence: true
-	
+
+    def totalprice
+    	quantity * price
+    end
 	enum unit: [:Pieces, :Kg]
 end

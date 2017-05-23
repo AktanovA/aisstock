266e97d8be1e0bbdbc3954c343205b09b97eb226
AktanovA <amir.aktanov@gmail.com>
2017-05-24 02:05:42 +0300

    Small changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521224521) do
+ActiveRecord::Schema.define(version: 20170518150649) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "title"
@@ -41,11 +41,6 @@ ActiveRecord::Schema.define(version: 20170521224521) do
     t.string   "title"
     t.string   "address"
     t.string   "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
